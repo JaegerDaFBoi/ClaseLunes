dd56a4779cd1c4671f1139ef9dfe3f018015fbf9
Juan Morales <mcallister2696@gmail.com>
2022-02-15 20:09:42 -0500

    metodo de busqueda por parametro

--- docs/funciones/funciones.php
+++ docs/funciones/funciones.php
@@ -1,5 +1,5 @@
 <?php
-
+    /*
     function listar()
     {
         $connection = mysqli_connect('localhost', 'root','','crud_adsi');
@@ -12,6 +12,21 @@
             echo $row['telefono']."<br>";
             echo $row['email']."<br>";
             echo "----------------------------"."<br>";
+        }
+    }
+    */
+    function listar()
+    {
+        $busqueda = "Agapito";
+        $connection = mysqli_connect('localhost', 'root','','crud_adsi');
+        $query = "SELECT * FROM contactos WHERE nombre LIKE '%$busqueda%'";
+        $data = mysqli_query($connection,$query,MYSQLI_ASSOC);
+        foreach ($data as $row) {
+            echo "El parametro de busqueda es: $busqueda"."<br>";
+            echo "ID: ".$row['id']."<br>";
+            echo "Nombre: ".$row['nombre']."<br>";
+            echo "Telefono: ".$row['telefono']."<br>";
+            echo "Email: ".$row['email']."<br>";
         }
     }
 ?>
